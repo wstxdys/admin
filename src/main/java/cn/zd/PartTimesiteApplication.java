073afabb2654de33d7465f50d01a2f049449cea9
wu <2112683943@qq.com>
2022-09-11 13:43:18 +0800

    初始化 二次

---
+++ src/main/java/cn/zd/PartTimesiteApplication.java
@@ -0,0 +1,13 @@
+package cn.zd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartTimesiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartTimesiteApplication.class, args);
+    }
+
+}
